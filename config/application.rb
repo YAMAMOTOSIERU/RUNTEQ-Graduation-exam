0f3995e262f2e2f41993f46b5031e9f8639cdccd
山本廉 <sieru67@gmail.com>
2024-05-07 11:43:32 +0900

    headerとfooter

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module App
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework nil
+    end
   end
 end
