7d6ca8da2785597cb3c3f1ea555e0f7e185dd809
山本廉 <sieru67@gmail.com>
2024-05-15 09:59:55 +0900

    掲示板作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user)
+    @posts = Post.includes(:user).order(created_at: :desc)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to posts_path, success: t('defaults.flash_message.created', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_created', item: Post.model_name.human)
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :weight)
   end
 end
