ad5048cb7b44c404dd966e8e8804e8dfef684bd0
山本廉 <sieru67@gmail.com>
2024-09-15 15:38:38 +0900

    nav修正

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,13 @@ import "./channels";
 const application = Application.start();
 
 console.log("こんにちは世界!");
+
+document.addEventListener("turbo:load", function () {
+  // Bootstrap のドロップダウンを初期化
+  var dropdownElementList = [].slice.call(
+    document.querySelectorAll(".dropdown-toggle")
+  );
+  var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl);
+  });
+});
